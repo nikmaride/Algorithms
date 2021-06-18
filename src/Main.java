9975f0c41d9f4cc736b37b98abe4d182959b21fd
nikmaride <nikmaden@yandex.ru>
2021-06-18 23:41:01 +0300

    .....

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int size = 5;
-        int array[] = new int[size];
+      
         }
